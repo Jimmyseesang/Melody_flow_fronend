263a96af9515cf2fc1be50b236a3f151b3ca2365
JimmySeesang <JimmySeesang@gmail.com>
2024-11-04 21:27:53 +0700

    Update input file admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -9,35 +9,50 @@ const AdminPage = () => {
     const [titleAlert, setTitleAlert] = useState(false)
     const [artistAlert, setArtistAlert] = useState(false)
     const [fileAlert, setFileAlert] = useState(false)
-    const formdata = new FormData()
 
     const dataChack = () => {
 
-        setTitleAlert(() => {title === '' || null ? true : false})
-        setArtistAlert(() => {artist === '' || null ? true : false})
-        setFileAlert(() => {file === '' || null ? true : false})
+        let status = true
+
+        if(title === '' || null) {
+            setTitleAlert(true)
+            status = false
+            
+        }
+        if(artist === '' || null) {
+            setArtistAlert(true)
+            status = false
+        }
+        if(file === '' || null) {
+            setFileAlert(true)
+            status = false
+        }
+
+        return status
 
     }
 
     const handleSubmit = (e) => {
 
         e.preventDefault()
-        dataChack()
-        console.log(title, artist, typeof(file), file)
+        if(dataChack()) {
+            const formdata = {title, artist, file}
+            console.log(formdata)
+        }
 
     }
 
     return(
         <div className="min-h-screen w-full bg-black-200/90 truncate">
             <NavBarComponent/>
-            <section className="w-full lg:mt-0 mt-[90px] md:mt-[150px]">
+            <section className="w-full lg:mt-0 mt-[150px] lg:mt-[90px]">
                 <div className="text-white mt-20">
                     <h1 className="text-center text-4xl font-bold text-pink-600">Admin</h1>
                 </div>
                 <div className="w-full flex justify-center items-center">
                     <form className="text-white" onSubmit={handleSubmit}>
                         <div className="flex flex-col my-4">
-                            <label htmlFor="music-title" className="label-style">Title <span className={`${titleAlert ? 'hidden' : ''}`}>alert  </span></label>
+                            <label htmlFor="music-title" className="label-style">Title</label>
                             <input 
                                 type="text" 
                                 placeholder="title..."
@@ -45,6 +60,7 @@ const AdminPage = () => {
                                 className="input-style"
                                 onChange={(e) => {setTitle(e.target.value)}}
                             />
+                            <p className={`text-red-700 ${titleAlert ? '' : 'hidden'}`}>Please enter this field!!!</p>
                         </div>
                         <div className="flex flex-col my-4">
                             <label htmlFor="artist" className="label-style">Artist <span className={`${artistAlert ? 'hidden' : ''}`}>alert</span></label>
@@ -55,15 +71,17 @@ const AdminPage = () => {
                                 className="input-style"
                                 onChange={(e) => {setArtist(e.target.value)}}
                             />
+                            <p className={`text-red-700 ${titleAlert ? '' : 'hidden'}`}>Please enter this field!!!</p>
                         </div>
                         <div className="flex flex-col my-4">
                             <label htmlFor="music-file" className="label-style">File <span className={`${fileAlert ? 'hidden' : ''}`}>alert</span></label>
                             <input 
                                 type="file" 
                                 id="music-file"
-                                className="bg-white p-2 rounded text-pink-500"
+                                className="bg-white p-2 rounded text-pink-500 hover:cursor-pointer"
                                 onChange={(e) => {setFile(e.target.files[0])}}
                             />
+                            <p className={`text-red-700 ${titleAlert ? '' : 'hidden'}`}>Please enter input file!!!</p>
                         </div>
                         <div className="flex justify-center items-center mt-8">
                             <button className="bg-pink-600 p-3 rounded w-1/2 hover:bg-black-200 hover:text-pink-600 hover:border-2 border-pink-600" type="submit">Enter</button>
